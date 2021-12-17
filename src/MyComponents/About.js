16c9fb254b68efc29899617e38e05b1c2242c2dc
Roshni <roshni.verma16@gmail.com>
2021-12-17 22:05:02 +0530

    first commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+           <h3 className='text-center'>About ToDo List</h3>
+            <p className='text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, enim. Nisi nostrum placeat laboriosam
+                 illo tempora atque facere exercitationem quam?</p>
+        </div>
+    )
+}
